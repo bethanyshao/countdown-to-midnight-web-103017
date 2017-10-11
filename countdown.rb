790cc93d1d2728d7049dd47583050d331cd6c84f
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-11 19:19:39 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(start_num)
+  current_num = start_num
+  while current_num > 0 do
+    puts "#{current_num} SECOND(S)!"
+    current_num -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
