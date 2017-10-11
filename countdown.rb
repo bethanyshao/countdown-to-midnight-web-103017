aae976d851cd7fe926fd3b836dcfb26cea162737
Bethany Shao <bethany.magdalena@gmail.com>
2017-10-11 19:21:08 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(start_num)
+  current_num = start_num
+  while current_num > 0 do
+    puts "#{current_num} SECOND(S)!"
+    current_num -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(start_num)
+  current_num = start_num
+  while current_num > 0 do
+    puts "#{current_num} SECOND(S)!"
+    current_num -= 1
+    sleep 1
+  end
+  "HAPPY NEW YEAR!"
 end
